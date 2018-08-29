bb78c3fff294edad5eda421c33b37d2bd3a50d18
Kenji Okimoto <okimoto@clear-code.com>
2018-08-29 15:36:28 +0900

    Add basic tests for FirefoxDriver

---
+++ test/test-firefox.lua
@@ -0,0 +1,30 @@
+local luaunit = require("luaunit")
+local FirefoxDriver = require("lib/firefox")
+
+TestFirefoxDriver = {}
+
+function TestFirefoxDriver:test_browser()
+  local driver = FirefoxDriver.new({})
+  luaunit.assert_equals(driver:browser(), "firefox")
+end
+
+function TestFirefoxDriver:test_default_options()
+  local driver = FirefoxDriver.new({})
+  luaunit.assert_equals(driver.base_url, "http://localhost:4444/")
+end
+
+function TestFirefoxDriver:test_wait_for_ready()
+  local driver = FirefoxDriver.new({})
+  luaunit.assert_equals(driver:wait_for_ready(), false)
+end
+
+function TestFirefoxDriver:test_start_without_callback()
+  local driver = FirefoxDriver.new({})
+  driver:start()
+  luaunit.assert_equals(driver:wait_for_ready(), true)
+  driver:stop()
+  local status = driver:status()
+  luaunit.assert_equals(status.ready, false)
+  luaunit.assert_equals(status.message, "Timeout: geckodriver may not be running")
+end
+
