cc673351a332bf90c84aa2d8605ba9faf7d99a7c
Kenji Okimoto <okimoto@clear-code.com>
2018-08-29 14:54:45 +0900

    Set default value as empty table

--- web-driver.lua
+++ web-driver.lua
@@ -6,7 +6,7 @@ web_driver.VERSION = "0.0.1"
 
 web_driver.create = function(browser, options)
   if browser == "firefox" then
-    return FirefoxDriver.new(options)
+    return FirefoxDriver.new(options or {})
   else
     -- unknown driver
     exit(1)
