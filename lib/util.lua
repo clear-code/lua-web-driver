352770c77eb082de6a8cc742512b303a4be52246
Kenji Okimoto <okimoto@clear-code.com>
2018-09-07 11:29:58 +0900

    Add util.inspect

--- lib/util.lua
+++ lib/util.lua
@@ -10,6 +10,10 @@ function util.p(root, options)
   print(inspect.inspect(root, options))
 end
 
+function util.inspect(root, options)
+  return inspect.inspect(root, options)
+end
+
 function util.element_id_from(id)
   return id["ELEMENT"] or id["element-6066-11e4-a52e-4f735466cecf"]
 end
