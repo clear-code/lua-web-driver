973289800dedc23fe50deb056731c98b022f22d1
Kenji Okimoto <okimoto@clear-code.com>
2018-09-07 11:29:32 +0900

    Add util.uuid()

--- lib/util.lua
+++ lib/util.lua
@@ -14,4 +14,17 @@ function util.element_id_from(id)
   return id["ELEMENT"] or id["element-6066-11e4-a52e-4f735466cecf"]
 end
 
+-- https://gist.github.com/jrus/3197011
+function util.uuid()
+  local template ="xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  function callback(c)
+    if c == "x" then
+      return string.format("%x", random(0, 0xf))
+    else
+      return string.format("%x", random(8, 0xb))
+    end
+  end
+  return template:gsub("[xy]", callback)
+end
+
 return util
