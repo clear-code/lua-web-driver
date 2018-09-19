c8a181948f5c57318a20918c4da3118ffba2a8af
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-09-19 12:24:44 +0900

    Modify to compare a text instead of comparing a json
    Because nil of Lua CJSON is "userdata : NULL".
    "userdata : NULL" is hard to compare in Lua.

--- test/test-element.lua
+++ test/test-element.lua
@@ -105,7 +105,7 @@ function TestElement:test_send_keys()
     local element = session:find_element("css selector", 'input[name=name]')
     local response = element:send_keys("This is test")
     luaunit.assert_equals(response.status_code, 200)
-    luaunit.assert_equals(response.json()["value"], nil)
+    luaunit.assert_equals(response.text, "{\"value\": null}")
     luaunit.assert_equals(element:get_property("value"), "This is test")
   end
   self.driver:start_session(callback)
