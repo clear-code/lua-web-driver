66cc571a8633c421810e9c56ea90921d009d9a74
Kouhei Sutou <kou@clear-code.com>
2018-09-21 10:49:23 +0900

    Make process start timeout customizable

--- web-driver/firefox.lua
+++ web-driver/firefox.lua
@@ -19,14 +19,15 @@ end
 local DEFAULT_HOST = "127.0.0.1"
 local DEFAULT_PORT = "4444"
 local DEFAULT_ARGS = { "-headless" }
+local DEFAULT_START_TIMEOUT = 5
 
 function methods:name()
   return "firefox"
 end
 
 local function ensure_running(firefox, geckodriver_process, geckodriver_command)
-  local timeout_sec = 1.0
-  local n_tries = 10
+  local timeout = firefox.start_timeout
+  local n_tries = 100
   for i = 1, n_tries do
     local success, _ = pcall(firefox.bridge.status, firefox.bridge)
     if success then
@@ -38,12 +39,12 @@ local function ensure_running(firefox, geckodriver_process, geckodriver_command)
       error("lua-web-driver: Firefox: " ..
               "Failed to run: <" .. geckodriver_command .. ">")
     end
-    process.nsleep((timeout_sec / n_tries) * (10 ^ 6))
+    process.nsleep((timeout / n_tries) * (10 ^ 6))
   end
   geckodriver_process:kill()
   process.waitpid(geckodriver_process:pid(), process.WNOHANG)
   error("lua-web-driver: Firefox: " ..
-          "Failed to run in " .. timeout_sec .. " seconds: " ..
+          "Failed to run in " .. timeout .. " seconds: " ..
           "<" .. geckodriver_command .. ">")
 end
 
@@ -102,6 +103,8 @@ local function apply_options(firefox, options)
       }
     }
   }
+
+  firefox.start_timeout = options.start_timeout or DEFAULT_START_TIMEOUT
 end
 
 function Firefox.new(options)
