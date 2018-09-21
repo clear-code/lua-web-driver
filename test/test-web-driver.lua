a5f165375344c4240207c7a87e30422271da0929
Kouhei Sutou <kou@clear-code.com>
2018-09-21 10:37:35 +0900

    Remove needless test

--- test/test-web-driver.lua
+++
@@ -1,9 +0,0 @@
-local luaunit = require("luaunit")
-local web_driver = require("web-driver")
-
-TestWebDriver = {}
-
-function TestWebDriver:test_firefox()
-  local driver = web_driver.Firefox.new()
-  luaunit.assert_equals(driver:browser(), "firefox")
-end
