305b51ffe76b5f48bb279096dea828ed2a20b4c3
Kouhei Sutou <kou@clear-code.com>
2018-09-21 14:29:37 +0900

    Update authors

--- web-driver.lua
+++ web-driver.lua
@@ -1,6 +1,8 @@
 --- WebDriver interface for Lua
 --
--- @author Kenji Okimoto
+-- @author Kenji Okimoto <okimoto@clear-code.com>
+-- @author Horimoto Yasuhiro <horimoto@clear-code.com>
+-- @author Kouhei Sutou <kou@clear-code.com>
 -- @copyright 2018
 -- @license MIT
 local web_driver = {}
