c6f51980ab60616bfa8d673d71eca2502f3e0bd0
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-09-26 16:55:44 +0900

    sample: add sample code for move on a website

---
+++ examples/move-to-a-website.lua
@@ -0,0 +1,20 @@
+local web_driver = require("web-driver")
+local driver = web_driver.Firefox.new()
+
+local callback = function(session)
+  session:navigate_to("http://localhost:10080/move.html")
+  local text_form = session:find_element("css selector", 'input[name=username]')
+  text_form:send_keys("username")
+  text_form = session:find_element("css selector", 'input[name=password]')
+  text_form:send_keys("password")
+  local button = session:find_element("css selector", "#button")
+  button:click()
+  local link = session:find_element("css selector", "a[name=link]")
+  link:click()
+  local element = session:find_element("css selector", "p")
+  print(element:text())
+end
+
+driver:start()
+driver:start_session(callback)
+driver:stop()
