e0d046670dc54767b17235925291d84d167adc97
Kouhei Sutou <kou@clear-code.com>
2018-09-26 11:04:19 +0900

    test: reuse web-driver/pp

--- test/helper.lua
+++ test/helper.lua
@@ -1,14 +1,12 @@
 local process = require("process")
 local requests = require("requests")
-local inspect = require("inspect")
+local pp = require("web-driver/pp")
 
 local helper = {}
 
 helper.PNG_HEADER = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }
 
-function helper.p(root, options)
-  print(inspect.inspect(root, options))
-end
+helper.p = pp.p
 
 function helper.start_server()
   local server = process.exec("ruby", { "-run", "-e", "httpd", "--", "--port", "10080", "test/fixtures" })
