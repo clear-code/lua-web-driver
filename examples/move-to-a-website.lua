a9dc265655d0f0c66c2338445f6887c4d4b6be15
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 13:54:00 +0900

    sample: simplify

--- examples/move-to-a-website.lua
+++ examples/move-to-a-website.lua
@@ -4,11 +4,11 @@ local driver = web_driver.Firefox.new()
 local URL =
   "https://clear-code.gitlab.io/lua-web-driver/sample/move.html"
 
-local callback = function(session)
+driver:start_session(function(session)
   session:navigate_to(URL)
   local text_form = session:find_element("css selector", 'input[name=username]')
   text_form:send_keys("username")
-  text_form = session:find_element("css selector", 'input[name=password]')
+  local text_form = session:find_element("css selector", 'input[name=password]')
   text_form:send_keys("password")
   local button = session:find_element("css selector", "#button")
   button:click()
@@ -16,8 +16,4 @@ local callback = function(session)
   link:click()
   local element = session:find_element("css selector", "p")
   print(element:text())
-end
-
-driver:start()
-driver:start_session(callback)
-driver:stop()
+end)
