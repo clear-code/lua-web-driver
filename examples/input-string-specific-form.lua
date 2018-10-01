6d20702074b0f6f5f4764d473f2f2f0d99393edd
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 12:34:50 +0900

    sample: add code for confirming input string

--- examples/input-string-specific-form.lua
+++ examples/input-string-specific-form.lua
@@ -4,12 +4,9 @@ local driver = web_driver.Firefox.new()
 local URL =
   "https://clear-code.gitlab.io/lua-web-driver/sample/index.html"
 
-local callback = function(session)
+driver:start_session(function(session)
   session:navigate_to(URL)
-  local element = session:find_element("css selector", 'input[name=name]')
-  element:send_keys("This is test")
-end
-
-driver:start()
-driver:start_session(callback)
-driver:stop()
+  local elements = session:css_select('input[name=name]')
+  elements[1]:send_keys("This is test")
+  print(elements[1]:get_property("value"))
+end)
