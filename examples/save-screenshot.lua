10f35932a5e88d97a207751188d876c0ce1a393f
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 12:45:12 +0900

    sample: Simplify

--- examples/save-screenshot.lua
+++ examples/save-screenshot.lua
@@ -4,13 +4,7 @@ local driver = web_driver.Firefox.new()
 local URL =
   "https://clear-code.gitlab.io/lua-web-driver/sample/index.html"
 
-function callback(session)
+driver:start_session(function(session)
   session:navigate_to(URL)
-  local png = session:take_screenshot()
-  io.output("sample.png")
-  io.write(png)
-end
-
-driver:start()
-driver:start_session(callback)
-driver:stop()
+  session:save_screenshot("sample.png")
+end)
