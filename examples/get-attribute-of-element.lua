5b6d18b643d941a7d5dfc62d3ec9e542fe29114f
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 15:47:53 +0900

    sample: use more meaningful sample case

--- examples/get-attribute-of-element.lua
+++ examples/get-attribute-of-element.lua
@@ -2,16 +2,14 @@ local web_driver = require("web-driver")
 local driver = web_driver.Firefox.new()
 
 local URL =
-  "https://clear-code.gitlab.io/lua-web-driver/sample/index.html"
+  "https://clear-code.gitlab.io/lua-web-driver/sample/get-attribute.html"
 
-local callback = function(session)
+driver:start_session(function(session)
   session:navigate_to(URL)
-  local element = session:find_element("css selector", "input[name=wine]")
-  local name = element:get_attribute("name")
-  local type = element:get_attribute("type")
-  print(name, type)
-end
-
-driver:start()
-driver:start_session(callback)
-driver:stop()
+  local elements = session:css_select('p')
+  for _, element in ipairs(elements) do
+    if element:get_attribute('data-value-type') == "number" then
+      print(element:text())
+    end
+  end
+end)
