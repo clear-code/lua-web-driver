7c46e304c3969dbcf6fc580d88bf6fb4187470a7
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 14:59:36 +0900

    sample: use more meaningful sample case

--- examples/move-to-a-website.lua
+++ examples/move-to-a-website.lua
@@ -7,12 +7,13 @@ local URL =
 driver:start_session(function(session)
   session:navigate_to(URL)
 
-  local text_form = session:css_select('input[name=username]')
+  local form = session:css_select('form')
+  local text_form = form:css_select('input[name=username]')
   text_form:send_keys("username")
-  local password_form = session:css_select('input[name=password]')
+  local password_form = form:css_select('input[name=password]')
   password_form:send_keys("password")
 
-  local button = session:css_select("#button")
+  local button = form:css_select("input[type=submit]")
   button:click()
 
   local link = session:link_search ("1")
