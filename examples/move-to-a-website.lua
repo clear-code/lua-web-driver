57d734c56dcb4d17b2c8b9b65676c43557f7df2e
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 13:56:30 +0900

    sample: fix wrong variable name

--- examples/move-to-a-website.lua
+++ examples/move-to-a-website.lua
@@ -6,12 +6,15 @@ local URL =
 
 driver:start_session(function(session)
   session:navigate_to(URL)
+
   local text_form = session:find_element("css selector", 'input[name=username]')
   text_form:send_keys("username")
-  local text_form = session:find_element("css selector", 'input[name=password]')
-  text_form:send_keys("password")
+  local password_form = session:find_element("css selector", 'input[name=password]')
+  password_form:send_keys("password")
+
   local button = session:find_element("css selector", "#button")
   button:click()
+
   local link = session:find_element("css selector", "a[name=1]")
   link:click()
   local element = session:find_element("css selector", "p")
