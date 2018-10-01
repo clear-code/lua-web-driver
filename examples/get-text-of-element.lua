c30e12ae0443281df9c02685e1ffb601d56f6349
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-01 10:12:57 +0900

    sample: modify access URL

--- examples/get-text-of-element.lua
+++ examples/get-text-of-element.lua
@@ -1,8 +1,11 @@
 local web_driver = require("web-driver")
 local driver = web_driver.Firefox.new()
 
+local URL =
+  "https://clear-code.gitlab.io/lua-web-driver/sample/index.html"
+
 local callback = function(session)
-  session:navigate_to("http://localhost:10080/index.html")
+  session:navigate_to(URL)
   local element = session:find_element("css selector", '#p2')
   local text = element:text()
   print(text)
