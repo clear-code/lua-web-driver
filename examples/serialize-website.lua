315fff1d398ecfc51832ef908c623210b989e0c2
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-10-02 09:18:14 +0900

    sample: simplify

--- examples/serialize-website.lua
+++ examples/serialize-website.lua
@@ -4,12 +4,8 @@ local driver = web_driver.Firefox.new()
 local URL =
   "https://clear-code.gitlab.io/lua-web-driver/sample/index.html"
 
-function callback(session)
+driver:start_session(function(session)
   session:navigate_to(URL)
   local xml = session:xml()
   print(xml)
-end
-
-driver:start()
-driver:start_session(callback)
-driver:stop()
+end)
