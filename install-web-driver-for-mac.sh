a1fe99ecee36b47c1c878a8f3d12c5c8ce8ebef5
Kouhei Sutou <kou@clear-code.com>
2018-10-03 11:27:32 +0900

    Use Homebrew to install geckodriver

--- install-web-driver-for-mac.sh
+++ install-web-driver-for-mac.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-GECKODRIVER_VERSION="0.22.0"
-curl -L -O https://github.com/mozilla/geckodriver/releases/download/v${GECKODRIVER_VERSION}/geckodriver-v${GECKODRIVER_VERSION}-macos.tar.gz
-tar xf geckodriver-v${GECKODRIVER_VERSION}-macos.tar.gz -C /usr/local/bin
-chmod +x /usr/local/bin/geckodriver
+brew install geckodriver
 
 LUAROCKS="sudo -H luarocks --tree=$(brew --prefix) --lua-dir=$(brew --prefix luajit)"
 ${LUAROCKS} install web-driver CRYPTO_DIR=$(brew --prefix openssl) OPENSSL_DIR=$(brew --prefix openssl)
